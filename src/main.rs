a788a4a5d7a4f182207d97e06715514eaff01ce5
Roy Stewart <roythomasstewart@gmail.com>
2022-08-07 00:07:15 -0400

    Added build configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo ) -> ! {
+    loop {}
+}
+
+#[lang = "eh_personality"]
+fn eh_personality() {}
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    loop {}
 }
